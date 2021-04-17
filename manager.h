8353aa4597bd58cea0caac2303067a7024ad2fa5
Yertle <kimpeter2018@naver.com>
2021-04-18 01:27:07 +0900

    add searchData func in manager.h

--- manager.h
+++ manager.h
@@ -4,3 +4,4 @@ int selectDataNo(Products *p[], int count);
 void listProducts(Products *p[], int count);
 int loadData(Products *p[]);
 void saveData(Products *p[], int count);
+void searchData(Products *p[], int count);
